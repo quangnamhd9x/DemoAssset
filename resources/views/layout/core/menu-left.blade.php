91f96f9a85124e3fc07e5d30ca720d2a59e06e5d
IT Robert <quangnamhd9x@gmail.com>
2020-11-25 11:32:23 +0700

    fix form login

--- resources/views/layout/core/menu-left.blade.php
+++ resources/views/layout/core/menu-left.blade.php
@@ -15,7 +15,7 @@
         <li class="nav-item active">
             <a class="nav-link" href="index.html">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
+                <span>Da</span></a>
         </li>
 
         <!-- Divider -->
